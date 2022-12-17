a0bd1a4989acf0dc00fc6c4771edd4dae881c129
switesh <88030988+switesh@users.noreply.github.com>
2022-12-17 22:36:59 +1100

    1 file modified

--- src/routes/business-models.svelte
+++ src/routes/business-models.svelte
@@ -16,6 +16,9 @@ let title = 'Business models'
     <h1>Business models</h1>
     <div class="flex gap-x-4">
       <input type="search" placeholder="Search business models">
+      <button class="btn base">
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>
+      </button>
       <button class="btn base">Suggest one for me</button>
     </div>
   </div>
